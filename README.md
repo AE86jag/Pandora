4828e15b814a6bb9c5be675fa05e901f42c28770
AE86 <15717003806@.com>
2022-06-28 21:08:08 +0800

    add api test

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
 # 万象盒子
 ## 架构
-DDD
-flyway
-SpringBoot
-mybatis
-mysql
 
 ## 版本变更记录 
 #### V1.0.0
-1. 上线功能
-
 -[x] 基金定投条件单
 -[x] 可转载中签登记
 -[x] 需求登记
--- src/test/java/com/pandora/DemandApiTest.java
+++ src/test/java/com/pandora/DemandApiTest.java
@@ -3,6 +3,7 @@ package com.pandora;
 import com.pandora.domain.demand.service.IDemandService;
 import com.pandora.domain.user.model.User;
 import com.pandora.infrastructure.interceptor.UserInfoContextHolder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,12 +18,13 @@ public class DemandApiTest extends BaseApiTest{
     private JdbcTemplate jdbcTemplate;
 
     @Test
+    @Ignore
     public void user_can_add_demand() {
         UserInfoContextHolder.set(User.mock());
         iDemandService.demandRegister("22", "223", "22234545");
         System.out.println(jdbcTemplate.queryForMap("select * from demand_register where name = '22'"));
 
         System.out.println("Test的线程ID: " + Thread.currentThread().getId());
-        //assertContract();
+        assertContract();
     }
 }
