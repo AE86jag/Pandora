01b6dd202671c872beca9fd52a121777dfa550b2
AE86 <15717003806@.com>
2022-07-24 16:18:36 +0800

    add http support

--- src/main/java/com/pandora/infrastructure/config/HttpConfig.java
+++ src/main/java/com/pandora/infrastructure/config/HttpConfig.java
@@ -6,23 +6,21 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class HttpConfig {
 
-    @Configuration
-    public class HttpsConfig {
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
 
-
-        @Bean
-        public ServletWebServerFactory serverFactory() {
-            TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-            tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-            return tomcat;
-        }
-
-        private Connector createStandardConnector() {
-            Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-            connector.setPort(8080);
-            return connector;
-        }
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
     }
 }
+
